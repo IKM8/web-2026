53cdef657d4aa0ec5536d3873688376069b893c7
Nikita <glushkow.nikita7v@gmail.com>
2026-03-17 17:46:09 +0300

    added ex4 of 4lab

---
+++ lab4/ex4.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> Задачи </title>
+    </head>
+    <body>
+        <form method="GET" action = "">
+            <p>Введите билеты</p>
+            <input type="number" name="number1" min="100000" max="999999">
+            <input type="number" name="number2" min="100000" max="999999">
+            <input type="submit" value="Вывести все cчастливые билеты в диапазоне">
+        </form>
+
+        <?php
+        if ((isset($_GET['number1'])) && (isset($_GET['number2']))) {
+            
+            $firstTicket = $_GET['number1'];
+            $secondTicket = $_GET['number2'];
+
+            function numberIsLucky($firstHalf, $secondHalf): bool {
+                $sum1 = 0;
+                $sum2 = 0;
+                for ($i = 0; $i < 3; $i++) {
+                    $sum1 += (int)$firstHalf[$i];
+                }
+                for ($i = 0; $i < 3; $i++) {
+                    $sum2 += (int)$secondHalf[$i];
+                }
+                if ($sum1 === $sum2) {
+                    return True;
+                }
+                else {
+                    return False;
+                }
+                
+            }
+            for ($i = (int) $firstTicket; $i < (int) $secondTicket; $i++) {
+                $firstHalfOfTicket = substr($i, 0, 3);
+                $secondHalfOfTicket = substr($i, 3, 3);
+                if (numberIsLucky($firstHalfOfTicket, $secondHalfOfTicket)) {
+                    echo $i . "<br>";
+                }
+            }
+        }
+        ?>
+    </body>
+</html>
